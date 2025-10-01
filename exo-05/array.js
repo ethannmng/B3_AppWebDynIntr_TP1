ec970a6b5c949aa601ea94496449309736651c89
ethannmng <ethanmng.pro@gmail.com>
2025-10-01 11:49:55 +0200

    Missions 1

---
+++ exo-05/array.js
@@ -0,0 +1,30 @@
+console.log('Tableaux :');
+
+// 1. Initialisation du tableau
+const arr = ["Initial"];
+
+console.log('Initialisation avec valeur initiale :');
+console.log(arr);
+
+// 2. Ecriture en index
+// [CODE]
+console.log('Ajout via écriture d\'index :');
+// [CODE]
+
+// 3. Ajout à la fin du tableau
+// [CODE]
+console.log('Ajout à la fin :');
+// [CODE]
+
+// 4. Ajout au début du tableau
+// [CODE]
+console.log("Ajout au début :"); 
+// [CODE]
+
+// 5. Modification de toutes les valeurs du tableau
+console.log("Modification de chaque valeur du tableau :");
+// [CODE]
+
+// 6. Filtrage pour ne garder que les valeurs numériques
+console.log("Ne garde que les valeurs numériques :");
+// [CODE]
