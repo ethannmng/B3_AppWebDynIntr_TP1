fd7f9d921dd207c10063a3a3a681a1be303ff00f
ethannmng <ethanmng.pro@gmail.com>
2025-10-01 14:50:14 +0200

    Init exo06

---
+++ exo-06/objects.js
@@ -0,0 +1,30 @@
+console.log("*** OBJETS ***");
+
+const obj = {
+  initialProp: "My initial property",
+  emptyProp: null,
+};
+console.log("Objet initial : ", obj);
+
+
+// 1. Ajout d'une propriété writtenProp
+// [CODE] 
+console.log("Ajout d'une propriété : ");
+// console.log(obj);
+
+// 2. Ajout d'une propriété imbriquée nestedProp
+// [CODE]
+console.log("Ajout d'une propriété imbriquée : ");
+// console.log(obj);
+
+// 3. Lecture de la propriété imbriquée
+console.log("Lecture d'une propriété imbriquée : ");
+// [CODE]
+
+// 4. Lecture des valeurs de l'objet avec une boucle for in
+console.log("Lecture d'un objet via for..in");
+// [CODE]
+
+// 5. Filtrage de l'objet pour retirer les valeurs nulles
+console.log("Objet sans props nulles : ");
+// [CODE]
