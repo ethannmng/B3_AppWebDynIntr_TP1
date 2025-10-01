7dab1bb1ae0b4500a1cce1ef14156a6fcf4a5312
ethannmng <ethanmng.pro@gmail.com>
2025-10-01 10:54:00 +0200

    Numbers.js completion

---
+++ exo-02/numbers.js
@@ -0,0 +1,17 @@
+let a = parseInt(1.42);
+let b = parseFloat(1.42);
+let c = parseInt("De toute évidence, je ne suis pas un nombre");
+let d = parseFloat("De toute évidence, je ne suis pas un nombre");
+
+// Exemple mission 2
+let e = parseInt("42");
+let f = parseFloat("42");
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+// Exemple mission 2
+console.log(e);
+console.log(f);
